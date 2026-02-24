b662e82ca3505c237682d22c5ec8d5598c371733
Danii Filipchik <feleiphcek@mmf.by>
2026-02-24 22:50:55 +0300

    add input quantity of players,skip btn,restart btn

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
 let CountPlayers=2;
-let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-let currentplayer = 1;
+let PlayersCards = [];
 
-renderCards();
-renderTurn();
+const setupBlock = document.querySelector("#setup");
+const gameContainer = document.querySelector("#game-container");
+const startBtn = document.querySelector("#startBtn");
+const playersInput = document.querySelector("#playersInput");
+
+
+startBtn.addEventListener("click", initGame);
+
+
+function initGame() {
+    CountPlayers = parseInt(playersInput.value);
+    
+    PlayersCards = [];
+    for (let i = 0; i < CountPlayers; i++) {
+        PlayersCards.push([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    }
+
+    currentplayer = 1;
+
+    setupBlock.style.display = "none";
+    gameContainer.style.display = "block";
+
+    renderCards();
+    renderTurn();
+}
 
 function removeCards(cardsToRemove) {
   // remove from cards
-  cards = cards.filter((card) => !cardsToRemove.includes(card));
-  
+
+  PlayersCards[currentplayer - 1] = PlayersCards[currentplayer - 1].filter(
+    (card) => !cardsToRemove.includes(card),
+  );
+  if (IsWin()) {
+    RestartGame();
+    renderCards();
+    return;
+  }
+  currentplayer == CountPlayers ? (currentplayer = 1) : currentplayer++;
 
   renderCards();
   renderTurn();
-  if(IsWin()){
-    RestartGame();
-  };
-  currentplayer == 1 ? currentplayer++ : currentplayer--;
 }
 
 function renderTurn() {
@@ -35,6 +61,8 @@ function renderTurn() {
 
   b2.innerText = d1 + d2;
 
+  updateButtonsState(d1,d2,b1,b2);
+
   b2.addEventListener("click", () => removeCards([d1 + d2]));
   b1.addEventListener("click", () => removeCards([d1, d2]));
   // Listen click and call removeCards([d1+d2])
@@ -44,66 +72,102 @@ function renderTurn() {
 
   choice.appendChild(b1);
   choice.appendChild(b2);
+
+  let skipbtn = document.createElement("button");
+  skipbtn.innerText = "Skip";
+  skipbtn.addEventListener("click", () => {
+    currentplayer == CountPlayers ? (currentplayer = 1) : currentplayer++;
+    renderTurn();
+    
+  });
+
+  choice.appendChild(skipbtn); 
 }
 
 function renderCards() {
   let cardsEl = document.querySelector("#cards");
   cardsEl.innerHTML = "";
 
-  for (let i = 0; i < 12; i++) {
-    let div = document.createElement("div");
-    div.innerText = i + 1;
+  for (let p = 0; p < CountPlayers; p++) {
+    let playercardsblock = document.createElement("div");
+    playercardsblock.innerHTML = `<h3>Игрок №${p + 1}</h3>`;
+    cardsEl.appendChild(playercardsblock);
 
-    cards.includes(i+1)?div.style.color = "red":div.style.color = "green"
-    
+    for (let i = 1; i <= 12; i++) {
+      let div = document.createElement("div");
+      div.innerText = i;
 
-    cardsEl.appendChild(div);
+      if (PlayersCards[p].includes(i)) {
+        div.style.color = "red";
+      } else {
+        div.style.color = "green";
+      }
+
+      playercardsblock.appendChild(div);
+    }
   }
 }
 
 function rollDice() {
-  return Math.round(Math.random() * 5+1);
+  return Math.round(Math.random() * 5 + 1);
 }
 
 function IsWin() {
-  if (cards.length === 0) {
-
+  if (PlayersCards[currentplayer - 1].length == 0) {
     let buttons = document.querySelectorAll("#choice button");
     buttons.forEach((btn) => {
       btn.disabled = true;
       btn.style.opacity = "0.5";
     });
 
-    let winblock = document.querySelector("#winblock")
+    let winblock = document.querySelector("#winblock");
     let winMessage = document.createElement("div");
     winMessage.style.textAlign = "center";
-    winMessage.innerHTML = `<h1 style="color: red;">Победил игрок № ${currentplayer==2?1:2}!</h1>`
-    
+    winMessage.innerHTML = `<h1 style="color: red;">Победил игрок № ${currentplayer}!</h1>`;
+
     winblock.appendChild(winMessage);
 
-    let player = document.querySelector("#player")
-    player.innerText= ""
+    let player = document.querySelector("#player");
+    player.innerText = "";
 
     return true;
   }
 }
 
-function RestartGame(){
-    let choice = document.querySelector("#choice");
+function RestartGame() {
+  let choice = document.querySelector("#choice");
 
-    let restartbtn=document.createElement("button")
-    restartbtn.innerText="restart game";
+  let restartbtn = document.createElement("button");
+  restartbtn.innerText = "restart game";
 
-    choice.appendChild(restartbtn);
+  choice.appendChild(restartbtn);
 
-    restartbtn.addEventListener("click",function(){
-        cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        renderCards();
-        renderTurn();
-        let winblock = document.querySelector("#winblock");
-        winblock.innerText= ""
-
-    })
-
+  restartbtn.addEventListener("click", function () {
+    PlayersCards.length = 0;
+    for (let i = 0; i < CountPlayers; i++) {
+      PlayersCards.push([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    }
+    currentplayer = 1;
+    renderCards();
+    renderTurn();
+    let winblock = document.querySelector("#winblock");
+    winblock.innerText = "";
+  });
+  
 }
+
+
+function updateButtonsState(d1, d2,b1,b2) {
+  const currentCards = PlayersCards[currentplayer - 1];
+  
+  let sumExists = currentCards.includes(d1 + d2);
+  b2.disabled = !sumExists; 
+  b2.style.opacity = sumExists ? "1" : "0.3";
+
+  
+  let bothExist = currentCards.includes(d1) || currentCards.includes(d2);
+  b1.disabled = !bothExist; 
+  b1.style.opacity = bothExist ? "1" : "0.3";
+}
+
 
